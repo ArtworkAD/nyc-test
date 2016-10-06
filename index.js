f348ca7bfbd889c1bf30820096f9a0385e506d01
ArtjomZab <artjomzabelin@gmail.com>
2016-10-06 10:22:02 +0200

    remove irrelevant code

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 // Load libraries
 const koa = require('koa');
-
-// Load api
-const api = require('./app/api.js');
-const options = require('./app/options.js');
-
 // Create app
 const app = module.exports.app = new koa();
 
